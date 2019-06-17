17838aab115a96673cfd9f5972271faa4c68228c
张啸 <petsgre@gmail.com>
2019-06-17 19:32:39 +0800

    配置nuxt并发布

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="test">
+    这是一个测试页面
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+.test {
+  color: red;
+}
+</style>
